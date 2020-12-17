5f13640ed41cac37cf78917a5a99bf00efa16881
Caspj <Caspj@CASPER>
2020-12-17 14:59:22 +0100

    Add project files.

---
+++ ThreadBasis4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadBasis4
+{
+    class Program
+    {
+        static char output = '*';
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                Thread printer = new Thread(Print);
+                Thread reader = new Thread(Read);
+                printer.Start();
+                reader.Start();
+            }
+        }
+
+        static void Print()
+        {
+            Console.Write(output);
+        }
+
+        static void Read()
+        {
+            if (!char.TryParse(Console.ReadLine(), out char input))
+            {
+                return;
+            }
+            output = input;
+        }
+    }
+}
